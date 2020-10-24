b7e1fb89ac2dae984c97f32d93e6b92292b921d9
Shiburaj Pappu <powerupneo@gmail.com>
2020-10-24 19:01:18 +0530

    Save to Local Storage completed

--- js/main.js
+++ js/main.js
@@ -1,3 +1,25 @@
+let todos = [
+    {
+        title: 'Drink Milk',
+        status: 'Pending'
+    },
+    {
+        title: 'Make Breakfast',
+        status: 'Pending'
+    },
+    {
+        title: 'Eat Lunch',
+        status: 'Complete'
+    },
+    {
+        title: 'Sleep',
+        status: 'Pending'
+    }
+];
+
+getTodosFromStorage();
+
+
 let todoForm = document.querySelector(".todo-form form");
 let htmlBody = document.querySelector(".todo-table table tbody");
 
@@ -8,36 +30,92 @@ todoForm.addEventListener('submit', function(e){
     
     addTodoItem(todoTitle);
 
+    renderTodo();
+    calculateTodoStatus();
+
     todoForm.todoname.value = '';
 });
 
 htmlBody.addEventListener('click', function(e){
     if(e.target.getAttribute('type')=='checkbox'){
-        let tr = e.target.parentNode.parentNode;
         if(e.target.checked){
-            tr.classList.add('complete');
-            tr.lastElementChild.innerHTML = 'Complete';
+            todos[e.target.id].status = 'Complete';
         }else{
-            tr.classList.remove('complete');
-            tr.lastElementChild.innerHTML = 'Pending';
+            todos[e.target.id].status = 'Pending';
         }
+        renderTodo();
+        calculateTodoStatus();
     }
 });
 
 htmlBody.addEventListener('dblclick', function(e){
-    console.log();
     if(e.target.nodeName=='TD'){
         let tr = e.target.parentNode;
-        tr.remove();
+        todos.splice(tr.firstElementChild.firstElementChild.id, 1);
+        renderTodo();
+        calculateTodoStatus();
     }
 });
 
-function addTodoItem(title){
-    let htmlContent = `<tr>
-                            <td><input type="checkbox" name="" id=""></td>
-                            <td>${title}</td>
-                            <td>Pending</td>
-                        </tr>`; 
 
-    htmlBody.innerHTML += htmlContent;
+function addTodoItem(title){
+    if(title==''){
+        return;
+    }
+    todos.push({
+        title,
+        status: 'Pending'
+    });
+}
+
+function calculateTodoStatus(){
+    let total = 0;
+    let complete = 0;
+    for(let todo of todos){
+        total++;
+        if(todo.status=='Complete'){
+            complete++;
+        }
+    }
+    let status = document.querySelector('.todo-table small');
+
+    if(total==complete){
+        status.innerHTML = `Congratulations!!! All Tasks completed.`;
+    }else{
+        status.innerHTML = `${total} Total, ${complete} Complete and ${total-complete} Pending`;
+    }
+}
+
+calculateTodoStatus();
+
+
+function renderTodo(){
+    htmlBody.innerHTML = '';
+    if(todos.length <=0){
+        htmlBody.innerHTML = `<tr>
+                                    <td colspan="3">No Todos added till now.</td>
+                                </tr>`; 
+    }
+    todos.forEach(function(todo,i){
+        htmlBody.innerHTML += `<tr class="${todo.status=='Complete'?'complete':''}">
+                                    <td><input type="checkbox" ${todo.status=='Complete'?'checked':''} name="" id="${i}"></td>
+                                    <td>${todo.title}</td>
+                                    <td>${todo.status}</td>
+                                </tr>`; 
+    });
+    saveToStorage();
+}
+
+renderTodo();
+
+function saveToStorage(){
+    localStorage.setItem('todos', JSON.stringify(todos));
+}
+
+function getTodosFromStorage(){
+    if(localStorage.getItem('todos')==null){
+        todos = [];
+    }else{
+        todos = JSON.parse(localStorage.getItem('todos'));
+    }
 }
